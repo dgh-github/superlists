223ce166df7d82834966cdb7d6d742052ee8f12c
dingguohui <dgh_apple@icloud.com>
2019-11-02 21:51:37 +0800

    从模板产生一个HTML页面。

--- functional_tests.py
+++ functional_tests.py
@@ -23,10 +23,10 @@ class NewVisitorTest(unittest.TestCase):
 
     # 应用邀请她输入一个待办事项
     inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
-    # 她在一个文本框中输入了“Buy peacock feathers”(购买孔雀羽毛)
+    self.assertEqual(inputbox.get_attribute('placeholder'),'输入一个待办事项')
+    # 她在一个文本框中输入了“购买孔雀羽毛”
     # 伊迪丝的爱好是使用假蝇做饵钓鱼
-    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys('购买孔雀羽毛')
 
     # 她按回车键后，页面更新了
     # 待办事项表格中显示了“1: Buy peacock feathers” 
@@ -35,7 +35,7 @@ class NewVisitorTest(unittest.TestCase):
 
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
-    #self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows)
+    self.assertTrue(any(row.text == '1: 购买孔雀羽毛' for row in rows),"新的待办事项没有加入表中。")
 
     # 页面中又显示了一个文本框，可以输入其他的待办事项
     # 她输入了“Use peacock feathers to make a fly”(使用孔雀羽毛做假蝇
