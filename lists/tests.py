8a848e5ab82dbd7759f730c8dd5bfece1ae96726
dingguohui <dgh_apple@icloud.com>
2019-11-02 20:41:50 +0800

    一种手动渲染模板的测试方法。

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -10,9 +11,8 @@ class HomePageTest(TestCase):
     self.assertEqual(found.func,home_page)
 
   def test_home_page_returns_correct_html(self):
-    request = HttpRequest()
-    response = home_page(request)
+    response = self.client.get('/')
     html = response.content.decode('utf-8')
-    self.assertTrue(html.startswith('<html>'))
-    self.assertIn('<title>待办事项清单</title>', html)
-    self.assertTrue(html.endswith('</html>'))
+    expected_html = render_to_string('home.html')
+    self.assertEqual(html,expected_html)
+    
