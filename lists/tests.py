39c24f4d25c8491b9308e07d7d35aad219375c9f
dingguohui <dgh_apple@icloud.com>
2019-11-02 21:14:50 +0800

    精简了单元测试项目。

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,8 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
-from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-  def test_root_url_resolve_to_home_page_view(self):
-    found = resolve('/')
-    self.assertEqual(found.func,home_page)
-
-  def test_home_page_returns_correct_html(self):
+  def test_user_home_template(self):
     response = self.client.get('/')
-    html = response.content.decode('utf-8')
-    self.assertTrue(html.startswith('<html>'))
-    self.assertIn('<title>待办事项清单</title>', html)
-    self.assertTrue(html.strip().endswith('</html>'))
     self.assertTemplateUsed(response, 'home.html')
     
