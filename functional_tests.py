c10bc527b7e7e0a2b2a5ebe8d389e7d32b64414b
dingguohui <dgh_apple@icloud.com>
2019-11-03 19:50:43 +0800

    对功能测试进行了小重构。

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def check_for_row_in_list_table(self,row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text,[row.text for row in rows])
+
   def test_can_start_a_list_and_retriever_it_later(self):
     # 伊迪丝听说有一个很酷的在线待办事项应用 
     # 她去看了这个应用的首页
@@ -29,17 +34,11 @@ class NewVisitorTest(unittest.TestCase):
     inputbox.send_keys('购买孔雀羽毛')
 
     # 她按回车键后，页面更新了
-    # 待办事项表格中显示了“1: Buy peacock feathers” 
+    # 待办事项表格中显示了“1: 购买孔雀羽毛” 
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
+    self.check_for_row_in_list_table('1: 购买孔雀羽毛')
 
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    #self.assertTrue(
-    #  any(row.text == '1: 购买孔雀羽毛' for row in rows),
-    #  f"新的待办事项没有出现在表中，内容是：\n{row.text}")
-
-    self.assertIn('1: 购买孔雀羽毛',[row.text for row in rows])
     # 页面中又显示了一个文本框，可以输入其他的待办事项
     # 她输入了“使用孔雀羽毛做假蝇”
     # 伊迪丝做事很有条理
@@ -49,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
     time.sleep(1)
 
     # 页面再次更新，她的清单中显示了这两个待办事项
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: 购买孔雀羽毛',[row.text for row in rows])
-    self.assertIn('2: 使用孔雀羽毛做假蝇',[row.text for row in rows])
+    self.check_for_row_in_list_table('1: 购买孔雀羽毛')
+    self.check_for_row_in_list_table('2: 使用孔雀羽毛做假蝇')
 
     # 伊迪丝想知道这个网站是否会记住她的清单 
     # 她看到网站为她生成了一个唯一的URL
