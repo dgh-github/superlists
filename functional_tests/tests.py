76d5b264c4680e52620dbfe5f220106366182d30
dingguohui <dgh_apple@icloud.com>
2019-11-05 19:21:17 +0800

    确保测试隔离，管理全局状态。

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT=10
 
 class NewVisitorTest(LiveServerTestCase):
   def setUp(self):
@@ -11,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
   def tearDown(self):
     self.browser.quit()
 
-  def check_for_row_in_list_table(self,row_text):
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn(row_text,[row.text for row in rows])
+  def wait_for_row_in_list_table(self,row_text):
+    start_time = time.time()
+    while True:
+      try:
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        return
+      except (AssertionError,WebDriverException) as e:
+        if time.time()-start_time > MAX_WAIT:
+          raise e
+        time.sleep(0.5)
 
   def test_can_start_a_list_and_retriever_it_later(self):
     # 伊迪丝听说有一个很酷的在线待办事项应用 
@@ -36,8 +47,7 @@ class NewVisitorTest(LiveServerTestCase):
     # 她按回车键后，页面更新了
     # 待办事项表格中显示了“1: 购买孔雀羽毛” 
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
-    self.check_for_row_in_list_table('1: 购买孔雀羽毛')
+    self.wait_for_row_in_list_table('1: 购买孔雀羽毛')
 
     # 页面中又显示了一个文本框，可以输入其他的待办事项
     # 她输入了“使用孔雀羽毛做假蝇”
@@ -45,11 +55,10 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('使用孔雀羽毛做假蝇')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
 
     # 页面再次更新，她的清单中显示了这两个待办事项
-    self.check_for_row_in_list_table('1: 购买孔雀羽毛')
-    self.check_for_row_in_list_table('2: 使用孔雀羽毛做假蝇')
+    self.wait_for_row_in_list_table('1: 购买孔雀羽毛')
+    self.wait_for_row_in_list_table('2: 使用孔雀羽毛做假蝇')
 
     # 伊迪丝想知道这个网站是否会记住她的清单 
     # 她看到网站为她生成了一个唯一的URL
