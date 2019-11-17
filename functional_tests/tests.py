7b213f49337cc2850fcee7dc9164651fb6f9d240
dingguohui <dgh_apple@icloud.com>
2019-11-17 12:31:22 +0800

    第一次对界面布局做功能测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -109,3 +109,20 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertIn('购买牛奶',page_text)
 
     # 两人都很满意，然后去睡觉了
+
+  def test_layout_and_styling(self):
+    #伊迪丝访问首页
+    self.browser.get(self.live_server_url)
+    self.browser.set_window_size(1024,769)
+
+    #她看到输入框完美地居中显示
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('测试')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('1: 测试')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertAlmostEqual(
+      inputbox.location['x'] + inputbox.size['width']/2,
+      512,
+      delta = 10
+    )
